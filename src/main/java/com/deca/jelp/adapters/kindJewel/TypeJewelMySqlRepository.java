073cbdc9248009a7bb85cc5aec3ca470db91669c
dalvarezavila <dalvarez@addi.com>
2021-04-03 22:41:47 -0500

    resolved coments

---
+++ src/main/java/com/deca/jelp/adapters/kindJewel/TypeJewelMySqlRepository.java
@@ -0,0 +1,24 @@
+package com.deca.jelp.adapters.kindJewel;
+
+import com.deca.jelp.domain.jewel.TypeJewel;
+import com.deca.jelp.domain.jewel.persistence.TypeJewelRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TypeJewelMySqlRepository implements TypeJewelRepository {
+
+    @Override
+    public List<TypeJewel> getTypes() {
+        List<TypeJewel> types = new ArrayList<>();
+        TypeJewel typeJewelOne = new TypeJewel("Manilla");
+        TypeJewel typeJewelTwo = new TypeJewel("Anillo");
+        TypeJewel typeJewelThree = new TypeJewel("Argolla");
+        types.add(typeJewelOne);
+        types.add(typeJewelTwo);
+        types.add(typeJewelThree);
+        return types;
+    }
+}
--- src/main/java/com/deca/jelp/domain/jewel/persistence/JewelRepository.java
+++ src/main/java/com/deca/jelp/domain/jewel/persistence/JewelRepository.java
@@ -5,6 +5,8 @@ import com.deca.jelp.domain.jewel.MaterialJewel;
 import com.deca.jelp.domain.jewel.TypeJewel;
 import com.deca.jelp.domain.jewel.WeightJewel;
 
+import java.util.List;
+
 public interface JewelRepository {
     void save(TypeJewel typeJewel, CaratsJewel caratsJewel, WeightJewel weightJewel, MaterialJewel materialJewel);
 }
