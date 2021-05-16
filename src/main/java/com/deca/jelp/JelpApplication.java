1992b944ebd273a4b58a160557098ab403a1b7a5
deaa1a <deaa1a@gmail.com>
2021-05-16 12:03:42 -0500

    compare eith main

--- src/main/java/com/deca/jelp/JelpApplication.java
+++ src/main/java/com/deca/jelp/JelpApplication.java
@@ -2,6 +2,7 @@ package com.deca.jelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class JelpApplication {
